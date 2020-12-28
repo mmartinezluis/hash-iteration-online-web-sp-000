0c08b4c9e23a7e5296d594ff269bbeff74d6be1c
Luis Martinez <luis_mmartinez@live.com>
2020-12-28 21:17:48 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,6 +7,6 @@
 def happy_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each do |name, age|
-    puts "Happy birthday #{name}! You are now #{age} old!"
+    puts "Happy birthday #{name}! You are now #{age} years old!"
   end
 end
